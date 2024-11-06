6ab1e5a1d3026bc05c460ed0d95560a75f6e63e0
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-11-06 12:05:16 -0500

    Ejercicio 4: Tablas del 1 al 10

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-numOne = int(input("Insert a number: "))
-numTwo = int(input("Insert a number: "))
-if numOne > numTwo:
-    numOne, numTwo = numTwo, numOne
-
-sum = int() #El error no era porque no existiese, era porque no se sabía aún el tipo de la variable.
-
-for i in range(numOne+1,numTwo):
-    sum += i
-
-print(f"The result is: {sum}")
+for i in range(1, 11):
     
+    row = [i * j for j in range(1, 11)]
+    
+    # Se usa f-string con un ancho de 3 caracteres para cada número 
+    # así se asegura el alineamiento a la derecha
+    print(" ".join(f"{num:3}" for num in row)) 
+    #print(row) # Fué para visualizar mejor lo que ocurre
