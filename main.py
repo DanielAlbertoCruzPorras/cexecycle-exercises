761c52e1fee0ad475683f74b6d05fd1851a450a2
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-11-06 12:16:39 -0500

    Ejercicio 5: Divisores

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-for i in range(1, 11):
-    
-    row = [i * j for j in range(1, 11)]
-    
-    # Se usa f-string con un ancho de 3 caracteres para cada número 
-    # así se asegura el alineamiento a la derecha
-    print(" ".join(f"{num:3}" for num in row)) 
-    #print(row) # Fué para visualizar mejor lo que ocurre
+num = int(input("Insert a number: "))
+div = []
+for i in range(1, num):
+    if num % i == 0:
+        div.append(i)
+
+div.append(num)
+
+print(" ".join(f"{num:3}" for num in div))
