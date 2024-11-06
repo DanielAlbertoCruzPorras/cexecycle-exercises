32ba94739c74de9141ac60c95f6d9570b3ab8988
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-11-06 12:38:08 -0500

    Ejercicio 7 parte 1: Rectángulo de asteriscos

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-b = True
-sum = float()
-while b:
-    tramo = int(input("Duracion tramo: "))
-    sum += tramo
-    if tramo == 0:
-        b = False
-print(f"Tiempo total del viaje: {sum//60:.0f}:{sum%60:.0f} horas")
+a = int(input("Altura: "))
+b = int(input("Ancho: "))
+for i in range(a):
+    print("".join(f"* " for j in range(b)))
