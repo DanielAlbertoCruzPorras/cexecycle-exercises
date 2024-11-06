3bfc5db233cb2fc7be7687bea9f82ebfe6db530a
DanielAlbertoCruzPorras <83321153+DanielAlbertoCruzPorras@users.noreply.github.com>
2024-11-06 11:35:10 -0500

    Motivo de error

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ numTwo = int(input("Insert a number: "))
 if numOne > numTwo:
     numOne, numTwo = numTwo, numOne
 
-sum = 0
+sum = int() #El error no era porque no existiese, era porque no se sabía aún el tipo de la variable.
 
 for i in range(numOne+1,numTwo):
     sum += i
